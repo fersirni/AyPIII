bcff1bd73b1099603934fcdcb144d867e9f8371c
Meze ♕❁ <37318048+MezeLaw@users.noreply.github.com>
2019-05-26 23:38:01 -0300

    Adelanto en deleteo. Faltaría probar y hacer la logica del tercer caso. Seguro no esta bien y hay cosas para corregir pero obvio escucho su feedback :D

--- BST/main.c
+++ BST/main.c
@@ -110,3 +110,47 @@ struct nodo *buscarPosicionParaInsertar(int valor, struct nodo **raiz) {
     }
     return posicion;
 }
+
+
+//Seguro hay que modificar, pero para seguir usando lo anterior, se me ocurrio que habria que indicar que hijo queres
+//revisar, al momento lo defini con 1 o 0. Es feo, pero es para ver que piensan :P 
+void eliminarNodo(struct nodo ** nodoPadre, int valorHijo){
+    // Issue = Deleteo de Nodos #10
+
+    struct nodo nodoAuxPadre;
+    struct nodo nodoAux;
+
+
+    if(valorHijo == 0){
+        struct nodo nodoAuxPadre = **nodoPadre;
+        struct nodo nodoAux = *nodoAuxPadre.izquierda;
+    } else{
+        struct nodo nodoAuxPadre = **nodoPadre;
+        struct nodo nodoAux = *nodoAuxPadre.derecha;
+    }
+
+
+    //El deleteo de nodos, en un arbol se puede analizar desde 3 variantes:
+
+    //1 - Borrar un Nodo sin hijos
+    //En teoría aca deleteo el valor nulleandolo, debo probar xD
+
+    if(nodoAux.izquierda == NULL && nodoAux.derecha == NULL){
+        nodoAux.valor == NULL;
+    }
+    //2 - Borrar un Nodo con un subárbol hijo
+    else if (nodoAux.izquierda != NULL || nodoAux.derecha != NULL) {
+        //Si es nodo izquierdo..
+        if(nodoAux.izquierda!= NULL && nodoAux.derecha == NULL
+        && nodoAux.izquierda->izquierda == NULL && nodoAux.izquierda->derecha == NULL){
+            nodoAuxPadre.izquierda  = nodoAux.izquierda;
+        }
+            //Es nodo derecho
+        else if(nodoAux.derecha != NULL && nodoAux.izquierda == NULL
+        && nodoAux.derecha->derecha == NULL && nodoAux.derecha->izquierda == NULL) {
+            nodoAuxPadre.derecha = nodoAux.derecha;
+        }
+    }
+    //3 - Borrar un Nodo con dos subárboles hijos
+        //Falta esta logica y probar!
+}
